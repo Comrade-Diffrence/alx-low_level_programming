b34c9c2745780555f25c8e52af64b75ea015ea24
Comrade-Diffrence <soremekunemaxs@gmail.com>
2022-09-09 05:20:05 -0700

    Q5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - starting point
+ * return - 0
+ */
+
+int main(void) /*main - Starting point*/
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); /*success*/
+}
