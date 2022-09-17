3b83f9938e46373a4129d0190d1b23ba06686457
Comrade-Diffrence <soremekunemaxs@gmail.com>
2022-09-16 23:02:24 -0700

    Q01

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 nclude "main.h"
+
 /**
  * print_alphabet - Print alphabet in lowercase using only `_putchar`
  * Return: 0
  */
 void print_alphabet(void)
 {
-	char c = 'a';
+	char letter;
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
 	_putchar('\n');
 }
