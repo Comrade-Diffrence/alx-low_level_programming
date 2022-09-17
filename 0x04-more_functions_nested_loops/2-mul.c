f84177d0ceb09d8c1a52918f2361b10a6127e680
Comrade-Diffrence <soremekunemaxs@gmail.com>
2022-09-16 17:01:05 -0700

    Q2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,12 @@
 #include "main.c"
 
 /**
- * mul - Multiples two integers
- * @a: The first integer to be multiplied.
- * @b: The second integer to be multiplied.
- *
+ * mul - a function that multiplies two integers.
+ * @a: An input integer
+ * @b: An input integerer
  * Return: The result of the multiplication.
  */
-int main(void)
+nt mul(int a, int b)
 {
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
-	return (0);
+	return (a * b);
 }
