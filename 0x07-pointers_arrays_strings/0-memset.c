1e68b1ac790cf004217bb27e0d078c3eebb6ace2
Comrade-Diffrence <soremekunemaxs@gmail.com>
2022-09-26 14:45:42 -0700

    Q000

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,13 +5,14 @@
  * @s: buffer array
  * @b: constant byte
  * @n: number of bytes of memory area to fill
- * Description: Fill the first `n` bytes of the memory area pointed to by `s` with the constant byte `b`.
+ * Description: Fill the first `n` bytes of the memory area.
  * Return: Pointer to memory area `s`
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
+
 	i = 0;
 	while (n > 0)
 	{
